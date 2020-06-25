9b22e5983efdc33e557c0893f99794904c89d08d
jaydeep6800 <jaydeep6800@gmail.com>
2020-06-25 14:23:15 +0530

    final app

--- app.js
+++ app.js
@@ -51,10 +51,10 @@ app.use("/api/chatbot", botRoutes);
 const port = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static( './frontend/build' ))
+  app.use(express.static( 'frontend/build' ))
 
   app.get('*',(req,res) => {
-    res.sendFile(path.join('./frontend','build','index.html')) // relative path
+    res.sendFile(path.join(__dirname,'frontend','build','index.html')) // relative path
   })
   
 
